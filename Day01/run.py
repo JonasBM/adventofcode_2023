a9ea7260cbd693c512a987029830dfc1a4ecc6e4
Jonas Burigo <jonas@jonasbm.com.br>
2023-12-03 15:43:55 -0300

    refactor: day one

--- Day01/run.py
+++ Day01/run.py
@@ -1,45 +1,71 @@
 import os
-import re
+
 
 script_directory = os.path.dirname(os.path.realpath(__file__))
 
-# hack to allow for twoneighthree type of thing
 number_map = {
-    'one': 'one1one',
-    'two': 'two2two',
-    'three': 'three3three',
-    'four': 'four4four',
-    'five' : 'five5five',
-    'six': 'six6six',
-    'seven': 'seven7seven',
-    'eight': 'eight8eight',
-    'nine': 'nine9nine'
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five' : '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
 }
-
 
 def replace_word_numbers(line:str):
     for key, value in number_map.items():
         line = line.replace(key, value)
     return line
 
-def get_number_from_line(line:str):
-    # comment next line to part one
-    line = replace_word_numbers(line)
-    digits_only = ''.join(re.findall(r'\d', line))
-    if len(digits_only) < 1:
-        raise ValueError('No digits found')
-    return int(f'{digits_only[0]}{digits_only[-1]}')
+def get_dirgit_from_word(line:str, i:int, reverse=False) -> str:
+    for key, value in number_map.items():
+        if reverse:
+            start, end = i - len(key) + 1, i + 1
+        else:
+            start, end = i, i + len(key)
+        if line[start:end] == key:
+            return value
+    return None
+
+def get_digits(line:str, reverse=False) -> (str, str):
+    part1 = None
+    part2 = None
+    if reverse:
+        start, end, step = len(line) - 1, -1, -1
+    else:
+        start, end, step = 0, len(line), 1
+    for i in range(start, end, step):
+        if line[i].isdigit():
+            part1 = line[i]
+            if part2 is None:
+                part2 = line[i]
+        if part2 is None:
+            part2 = get_dirgit_from_word(line, i, reverse)
+        if part1 is not None and part2 is not None:
+            return part1, part2
+    raise ValueError('No digits found')
+
+
+def get_numbers_from_line(line:str) -> (int, int):
+    first1, first2 = get_digits(line)
+    last1, last2 = get_digits(line, reverse=True)
+    return int(f'{first1}{last1}'), int(f'{first2}{last2}')
 
 def run():
     print('run')
     input_path = os.path.join(script_directory, 'input.txt')
-    result = 0
+    result1 = 0
+    result2 = 0
     with open(input_path, 'r', encoding='utf-8') as f:
         for line in f:
-            number = get_number_from_line(line.strip().lower())
-            result += number
-    print('sum:', result)
-    return result
+            number1, number2 = get_numbers_from_line(line.strip().lower())
+            result1 += number1
+            result2 += number2
+    print('part 1:', result1) # 53334
+    print('part 2:', result2) # 52834
 
 if __name__ == '__main__':
     print('starting Day 01')
